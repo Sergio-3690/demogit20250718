522f8a726c6e74208d3fcac40ed70f123288b62b
Sergio-3690 <adityasanap196@gmail.com>
2025-10-25 18:01:04 +0530

    fn

--- add.java
+++ add.java
@@ -3,3 +3,24 @@ public class add {
         System.out.println("Hello, World!");
     }
 }
+
+
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int sum = num1 + num2;
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
+
+
+public class Factorial {
+    public static void main(String[] args) {
+        int num = 5, fact = 1;
+        for (int i = 1; i <= num; i++)
+            fact *= i;
+        System.out.println("Factorial of " + num + " is " + fact);
+    }
+}
